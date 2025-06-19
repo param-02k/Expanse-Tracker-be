e7116d5b2b5db4c9b31c45934ae35f5991468dd7
param-02k <priyam6280@gmail.com>
2025-06-19 18:44:48 +0530

    update email validate

--- model/userModel.js
+++ model/userModel.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose")
 const UserSchema = mongoose.Schema(
     {
         fullname : {type : String, required : true},
-        email : {type : String, required : true},
-        mobilenumber : {type : Number, required : true},
-        username : {type : String, required : true},
-        password : {type : String, required : true, match: [
+        email : {type : String, required : true, match: [
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
         "Please fill a valid email address"
-      ] }
+      ]},
+        mobilenumber : {type : Number, required : true},
+        username : {type : String, required : true},
+        password : {type : String, required : true}
     },{
     timestamps : true
 })
